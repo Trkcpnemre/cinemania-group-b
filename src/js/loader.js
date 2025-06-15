2cd24e3467c9866bc58c18ab5ce5dc1e05f785f5
furkancetin <furkan130526@gmail.com>
2025-06-15 19:19:44 +0300

    first commit

---
+++ src/js/loader.js
@@ -0,0 +1,15 @@
+async function loadHTML(id, filePath) {
+  const container = document.getElementById(id);
+  try {
+    const res = await fetch(filePath);
+    const html = await res.text();
+    container.innerHTML = html;
+  } catch (err) {
+    container.innerHTML = `<p style="color:red;">${filePath} yüklenemedi.</p>`;
+    console.error(`Hata: ${filePath}`, err);
+  }
+}
+
+// Parçaları yükle
+loadHTML("header", "./partials/header.html");
+loadHTML("library-list", "./partials/library-list.html");
