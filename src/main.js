5f68b8d6b560633118070286a4a8d503cfdb2c75
Gozde <gozdenurarici@gmail.com>
2025-06-17 23:53:21 +0300

    catalog gallery js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { fetchTrendingMovie } from './js/hero.js';
 import { loadHTML } from './js/loader.js';
+import { loadMovies } from './js/catalog.js'
 
 
 
@@ -9,6 +10,8 @@ loadHTML("header", "./partials/header.html");
 document.addEventListener('DOMContentLoaded', async () => {
     
     await loadHTML("hero", "./partials/hero.html");
+    await loadHTML("catalog-gallery", "./partials/catalog-gallery.html");
+    await loadMovies();
     await fetchTrendingMovie();
     await fetchCategories();
 });
